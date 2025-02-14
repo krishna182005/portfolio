fa1eaa35325b09a7cc0ab059e3ca206abdf117e0
krishna182005 <krishnavembuli@gmail.com>
2025-02-15 01:17:57 +0530

    Update E-commerce.js

--- E-commerce.js
+++ E-commerce.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let cartCount = 0;
-    const cartButton = document.querySelector(".cta");
-    const buyButtons = document.querySelectorAll(".buy");
 
-    buyButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            cartCount++;
-            cartButton.textContent = `Cart (${cartCount})`;
-        });
-    });
-});
