29e80c37f8becc17a229281c06f26fb19a2e4dc6
Friendlywebdeveloper2025 <krishnavembuli@gmail.com>
2025-02-18 18:08:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleDetails() {
     const details = document.querySelector('.details');
     const btn = document.querySelector('.show-more-btn');
 
-    // Toggle the visibility of the details section
+    
     details.classList.toggle('show');
 
     // Change button text based on visibility of details
